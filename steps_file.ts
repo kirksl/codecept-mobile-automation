836b690b1a282d875508f76df0cee25641cbe6b9
Kirk Slota <kirksl@gmail.com>
2022-10-11 18:43:07 -0700

    updates

--- steps_file.ts
+++ steps_file.ts
@@ -1,8 +1,6 @@
 // extend 'I' with actions that are site/application wide (global)
 // use 'this' instead of 'I' in this file to access it's methods
 
-const { I } = inject();
-
 export = function(): any {
     return actor({
         doSomethingGlobal: async function(): Promise<boolean> {
